44c267d2b6983a660cad11fa8f762473889a8f96
AdrianGL <55089389+GlAdrian-22@users.noreply.github.com>
2020-10-02 10:38:43 -0500

    MVS

---
+++ contenido.php
@@ -0,0 +1,29 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/halo.png"."||".
+				  "Halo"."||".
+				  "Imagen representativa de un Halo, perteneciente al videojuego Halo Infinite.";
+
+		$datos[1]="img/haloodst.jpg"."||".
+				  "ODST"."||".
+				  "Personaje de la serie del videojuego Halo 3 OSDT";
+
+		$datos[2]="img/starwars.jpg"."||".
+				  "Star Wars"."||".
+				  "Wallpaper de las diferentes zonas de la Serie Star Wars.
+";
+
+		$datos[3]="img/darkforce.jpg"."||".
+				  "Dark Force"."||".
+				  "Personajes representativos de la serie Star Wars";
+
+		$datos[4]="img/darthvader.jpg"."||".
+				  "DarthVader"."||".
+				  "Personaje iconico de las peliculas Star Wars.";
+		return $datos;
+	}
+
+ ?>
